b39bb47f22ee2da92f5ca89668a788acd4cce622
1umarc <luven04@gmail.com>
2025-04-02 16:38:30 +0800

    main template

---
+++ array.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+class Array
+{
+    public:
+        void insert(int);
+        void sort_lm(int);
+        void search_lm(int);
+        void sort_ck(int);
+        void search_ck(int);
+        void sort_xw(int);
+        void search_xw(int);
+        void sort_dn(int);
+        void search_dn(int);
+};
+
+void Array::insert(int a)
+{
+    // convert csv to array
+    // clean
+}
+
+void Array::sort_lm(int a)
+{
+    // Implementation
+}
+
+void Array::search_lm(int a)
+{
+    // Implementation
+}
+
+void Array::sort_ck(int a)
+{
+    // Implementation
+}
+
+void Array::search_ck(int a)
+{
+    // Implementation
+}
+void Array::sort_xw(int a)
+{
+    // Implementation
+}
+
+void Array::search_xw(int a)
+{
+    // Implementation
+}
+
+void Array::sort_dn(int a)
+{
+    // Implementation
+}
+
+void Array::search_dn(int a)
+{
+    // Implementation
+}
+
+
+int main()
+{
+    // #1
+
+    // #2
+
+    // #3
+    
+    return 0;
+}
---
+++ linkedlist.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+class LinkedList
+{
+    public:
+        void insert(int);
+        void sort_lm(int);
+        void search_lm(int);
+        void sort_ck(int);
+        void search_ck(int);
+        void sort_xw(int);
+        void search_xw(int);
+        void sort_dn(int);
+        void search_dn(int);
+};
+
+void LinkedList::insert(int a)
+{
+    // convert csv to array
+    // clean
+}
+
+void LinkedList::sort_lm(int a)
+{
+    // Implementation
+}
+
+void LinkedList::search_lm(int a)
+{
+    // Implementation
+}
+
+void LinkedList::sort_ck(int a)
+{
+    // Implementation
+}
+
+void LinkedList::search_ck(int a)
+{
+    // Implementation
+}
+
+void LinkedList::sort_xw(int a)
+{
+    // Implementation
+}
+
+void LinkedList::search_xw(int a)
+{
+    // Implementation
+}
+
+void LinkedList::sort_dn(int a)
+{
+    // Implementation
+}
+
+void LinkedList::search_dn(int a)
+{
+    // Implementation
+}
+
+
+int main()
+{
+    // #1
+
+    // #2
+
+    // #3
+    
+    return 0;
+}
