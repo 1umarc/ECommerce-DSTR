3cc37385ed94035d9b2fa7d4b2dbb37670d7248e
1umarc <luven04@gmail.com>
2025-04-03 00:42:23 +0800

    mergeSort v1

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -8,7 +8,7 @@ using namespace std;
 struct Node
 {
     int data;
-    Node* next;
+    Node* next; // Pointer to the next node's memory address
 };
 
 
@@ -18,30 +18,125 @@ class LinkedList
 public:
     Node* head;
 
+    // Constructor - Initializes empty first node
     LinkedList()
     {
-        head = NULL;
+        head = nullptr;
     } 
 
+    // Deconstructor - Mandatory to free memory as using heap
+    ~LinkedList()
+    {
+        // Retrieves head, store address of next node, deletes, then repeats
+        Node* delNode = head;
+        while (delNode != nullptr) 
+        {
+            Node* next = delNode->next;
+            delete delNode;
+            delNode = next;
+        }
+    }
+
+    // Inserting nodes to form Linked List - O(1)
     void insert(int data)
     {
+        // Creates new node which is the new head as it inserts from beginning
         Node* newHead = new Node();
-        newHead-> data = data;
+        newHead->data = data;
         newHead->next = head;
         head = newHead;
     }
 
+    // Print the Linked List
     void display() 
     {
+        // Traverses from head to NULL
         Node* temp = head;
 
-        while (temp != NULL) 
+        while (temp != nullptr) 
         {
             cout << temp->data << " -> ";
             temp = temp->next;
         }
         cout << "NULL" << endl;
     }
+
+
+    /* Merge Sort Implementation
+    - Time Complexity: O(n log n)
+    - Space Complexity: O(n)
+    - divide & conquer algorithm, same if linear or linked list
+    */
+    // Merges two sorted Lists - O(n)
+    Node* merge(Node *left, Node *right)
+    {
+        // At Base - If either list is empty, return the other
+        if(left == nullptr)
+        {
+            return right;
+        }
+        else if (right == nullptr)
+        {
+            return left;
+        }
+
+        // During Recursive - Compare data, smaller node attaches to the bigger node
+        if(left->data < right->data)
+        {
+            left->next = merge(left->next, right);
+            return left;
+        }
+        else
+        {
+            right->next = merge(left, right->next);
+            return right;
+        }
+    }
+
+    // Finds middle of the List with the slow-fast pointer approach - O(n)
+    Node* midSplit(Node *head)
+    {
+        Node* slow = head;
+        Node* fast = head->next;
+
+        // Slow pointer moves 1 step, fast pointer moves 2 steps
+        while(fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow; // When fast pointer at the end, slow pointer is at mid-point
+    }
+
+    // Function call for Merge Sort
+    void mergeSort()
+    {
+        head = mergeSort(head);
+    }
+
+
+private:
+    // Overload function to Sort the List - O(n log n)
+    Node* mergeSort(Node *head)
+    {
+        // At Base - If head or next to head is null, return
+        if(head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+
+        // During Recursive - Split the list, recursively sort both halves and merge  
+        Node* mid = midSplit(head);
+        Node* left = head;
+        Node* right = mid->next; // mid-point belongs to left, mid->next is the second half
+        mid->next = nullptr;
+    
+        left = mergeSort(left);
+        right = mergeSort(right);
+    
+        return merge(left, right);
+    }
+
 
     void sort_lm(int);
     void search_lm(int);
@@ -96,28 +191,31 @@ void LinkedList::search_dn(int a)
 
 int main()
 {
-    LinkedList transactions;
-    ifstream file("transactions.csv");
-    // Gets values but meant for int...
-    string line;
-    while (getline(file, line)) 
-    {
-        stringstream ss(line);
-        int value;
-        if (ss >> value)
-        {
-            transactions.insert(value);
-        }
-    }
-    file.close();
+    // LinkedList transactions;
+    // ifstream file("transactions.csv");
+    // .
+    // string line;
+    // while (getline(file, line)) 
+    // {
+    //     stringstream ss(line);
+    //     int value;
+    //     if (ss >> value)
+    //     {
+    //         transactions.insert(value);
+    //     }
+    // }
+    // file.close();
     
-    transactions.display();
+    // transactions.display();
 
     LinkedList l;
     l.insert(10);
     l.insert(14);
     l.insert(13);
     l.insert(12);
+    l.display();
+
+    l.mergeSort();
     l.display();
     //LM
     // #1
---
+++ linkedlist_lm.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+// Node structure for Singly Linked List
+struct Node
+{
+    int data;
+    Node* next; // Pointer to the next node's memory address
+};
+
+
+// Linked List class
+class LinkedList
+{
+public:
+    Node* head;
+
+    // Constructor - Initializes empty first node
+    LinkedList()
+    {
+        head = nullptr;
+    } 
+
+    // Deconstructor - Mandatory to free memory as using heap
+    ~LinkedList()
+    {
+        // Retrieves head, store address of next node, deletes, then repeats
+        Node* delNode = head;
+        while (delNode != nullptr) 
+        {
+            Node* next = delNode->next;
+            delete delNode;
+            delNode = next;
+        }
+    }
+
+    // Inserting nodes to form Linked List - O(1)
+    void insert(int data)
+    {
+        // Creates new node which is the new head as it inserts from beginning
+        Node* newHead = new Node();
+        newHead->data = data;
+        newHead->next = head;
+        head = newHead;
+    }
+
+    // Print the Linked List
+    void display() 
+    {
+        // Traverses from head to NULL
+        Node* temp = head;
+
+        while (temp != nullptr) 
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+
+    /* Merge Sort Implementation
+    - Time Complexity: O(n log n)
+    - Space Complexity: O(n)
+    - divide & conquer algorithm, same if linear or linked list
+    */
+    // Merges two sorted Lists - O(n)
+    Node* merge(Node *left, Node *right)
+    {
+        // At Base - If either list is empty, return the other
+        if(left == nullptr)
+        {
+            return right;
+        }
+        else if (right == nullptr)
+        {
+            return left;
+        }
+
+        // During Recursive - Compare data, smaller node attaches to the bigger node
+        if(left->data < right->data)
+        {
+            left->next = merge(left->next, right);
+            return left;
+        }
+        else
+        {
+            right->next = merge(left, right->next);
+            return right;
+        }
+    }
+
+    // Finds middle of the List with the slow-fast pointer approach - O(n)
+    Node* midSplit(Node *head)
+    {
+        Node* slow = head;
+        Node* fast = head->next;
+
+        // Slow pointer moves 1 step, fast pointer moves 2 steps
+        while(fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow; // When fast pointer at the end, slow pointer is at mid-point
+    }
+
+    // Function call for Merge Sort
+    void mergeSort()
+    {
+        head = mergeSort(head);
+    }
+
+
+private:
+    // Overload function to Sort the List - O(n log n)
+    Node* mergeSort(Node *head)
+    {
+        // At Base - If head or next to head is null, return
+        if(head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+
+        // During Recursive - Split the list, recursively sort both halves and merge  
+        Node* mid = midSplit(head);
+        Node* left = head;
+        Node* right = mid->next; // mid-point belongs to left, mid->next is the second half
+        mid->next = nullptr;
+    
+        left = mergeSort(left);
+        right = mergeSort(right);
+    
+        return merge(left, right);
+    }
+
+    
+    // // Sequential search implementation
+    // Node<T>* sequentialSearch(T key) {
+    //     Node<T>* temp = head;
+    //     while (temp) {
+    //         if (temp->data == key) return temp;
+    //         temp = temp->next;
+    //     }
+    //     return nullptr;
+    // }
+};
+
+
+int main()
+{
+    LinkedList l;
+    l.insert(10);
+    l.insert(14);
+    l.insert(13);
+    l.insert(12);
+    l.display();
+
+    l.mergeSort();
+    l.display();
+
+    // int searchValue = 500; // Example
+    // if (transactions.sequentialSearch(searchValue))
+    //     cout << "Value found!\n";
+    // else
+    //     cout << "Value not found!\n";
+    
+    return 0;
+}
