27513b54feead6660637e1e42116acf4e81588c1
1umarc <luven04@gmail.com>
2025-04-02 21:35:40 +0800

    linkedlist basic

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,30 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
+using namespace std;
+
+// Node structure for Singly Linked List
 struct Node
 {
     int data;
     Node* next;
 };
 
+
+// Linked List class
 class LinkedList
 {
-    public:
-        void insert(int);
-        void sort_lm(int);
-        void search_lm(int);
-        void sort_ck(int);
-        void search_ck(int);
-        void sort_xw(int);
-        void search_xw(int);
-        void sort_dn(int);
-        void search_dn(int);
-};
+public:
+    Node* head;
 
-void LinkedList::insert(int a)
-{
-    // convert csv to array
-    // clean
-}
+    LinkedList()
+    {
+        head = NULL;
+    } 
+
+    void insert(int data)
+    {
+        Node* newHead = new Node();
+        newHead-> data = data;
+        newHead->next = head;
+        head = newHead;
+    }
+
+    void display() 
+    {
+        Node* temp = head;
+
+        while (temp != NULL) 
+        {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    void sort_lm(int);
+    void search_lm(int);
+    void sort_ck(int);
+    void search_ck(int);
+    void sort_xw(int);
+    void search_xw(int);
+    void sort_dn(int);
+    void search_dn(int);
+};
 
 void LinkedList::sort_lm(int a)
 {
@@ -69,6 +96,29 @@ void LinkedList::search_dn(int a)
 
 int main()
 {
+    LinkedList transactions;
+    ifstream file("transactions.csv");
+    // Gets values but meant for int...
+    string line;
+    while (getline(file, line)) 
+    {
+        stringstream ss(line);
+        int value;
+        if (ss >> value)
+        {
+            transactions.insert(value);
+        }
+    }
+    file.close();
+    
+    transactions.display();
+
+    LinkedList l;
+    l.insert(10);
+    l.insert(14);
+    l.insert(13);
+    l.insert(12);
+    l.display();
     //LM
     // #1
     // #2
