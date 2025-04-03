3eb0f68cee4c25e6da636477608ca3594e1dfec4
dnsya <dn.nesya@gmail.com>
2025-04-03 16:29:51 +0700

    binary search code added

--- array_dn.cpp
+++ array_dn.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
-
 // binary search, heap sort
+
+int binarysearch(int ar[], int num, int first, int last) {
+    // repeat until pointers first and last meet each other
+    while (first <= last) {
+        int mid = first + (last - first) / 2;
+        if (num == ar[mid]) {
+            return mid;
+        }
+        else if (num > ar[mid]) {
+            first = mid + 1;
+        }
+        else {
+            last = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    
+    int ar[] = {23, 20, 15, 25, 13, 8, 18};
+    int num = 20;
+    int arsize = sizeof(ar) / sizeof(ar[0]);
+    int found = binarysearch(ar, num, 0, arsize - 1);
+    if (found == -1){
+        cout << num << " is not found is array.";
+    }
+    else {
+        cout << num << " is location at index " << found;
+    }
 }
