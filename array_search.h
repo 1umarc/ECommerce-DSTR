35a532d573a558193bfa80f1ac9feb15d4649ca8
1umarc <luven04@gmail.com>
2025-05-05 15:14:21 +0800

    last v1

--- array_search.h
+++ array_search.h
@@ -188,157 +188,60 @@ int Array_Search::linearSearch(ReviewBucket& bucket, const string& field1, const
 */
 int Array_Search::binarySearch(TransactionBucket& bucket, const string& field1, const string& value1, const string& field2, const string& value2) 
 {
-    if (bucket.size == 0)
+    int count = 0;
+    
+    for (int i = 0; i < bucket.size; i++) 
     {
-        return 0;
-    }
-
-    auto getFieldValue = [](const Transaction& transaction, const string& field) -> string 
-    {
-        if (field == "category") 
-        {
-            return transaction.category;
-        }
-        else if (field == "paymentMethod") 
-        {
-            return transaction.paymentMethod;
-        }
-        return "";
-    };
-
-    TransactionBucket filteredBucket;
-    filteredBucket.capacity = bucket.size;
-    filteredBucket.data = new Transaction[filteredBucket.capacity];
-    filteredBucket.size = 0;
-
-    int left = 0;
-    int right = bucket.size - 1;
-    int firstMatchIndex = -1;
-
-    while (left <= right) 
-    {
-        int mid = left + (right - left) / 2;
-        string currentValue = getFieldValue(bucket.data[mid], field1);
-
-        if (currentValue == value1) 
-        {
-            firstMatchIndex = mid;
-            right = mid - 1;  
-        } 
-        else if (currentValue < value1) 
-        {
-            left = mid + 1;
-        } 
-        else 
-        {
-            right = mid - 1;
-        }
-    }
-
-    if (firstMatchIndex == -1) 
-    {
-        delete[] bucket.data;
-        bucket.data = new Transaction[bucket.capacity];
-        bucket.size = 0;
-        return 0;
-    }
-
-    int i = firstMatchIndex;
-    while (i > 0 && getFieldValue(bucket.data[i-1], field1) == value1) 
-    {
-        i--;
-    }
-
-    while (i < bucket.size && getFieldValue(bucket.data[i], field1) == value1) 
-    {
+        bool match1 = false;
         bool match2 = true;
-        if (!field2.empty()) 
+
+        if (field1 == "category")
         {
-            match2 = (getFieldValue(bucket.data[i], field2) == value2);
+            match1 = (bucket.data[i].category == value1);
+        }
+        else if (field1 == "paymentMethod")
+        {
+            match1 = (bucket.data[i].paymentMethod == value1);
         }
 
-        if (match2) 
+        if (match1 && !field2.empty())
         {
-            filteredBucket.data[filteredBucket.size++] = bucket.data[i];
+            match2 = false;
+            if (field2 == "category")
+            {
+                match2 = (bucket.data[i].category == value2);
+            }
+            else if (field2 == "paymentMethod")
+            {
+                match2 = (bucket.data[i].paymentMethod == value2);
+            }
         }
-        i++;
+
+        if (match1 && match2)
+        {
+            count++;
+        }
     }
-
-    int count = filteredBucket.size;
-    delete[] bucket.data;
-    bucket.data = filteredBucket.data;
-    bucket.size = count;
-    
     return count;
-}   
-int Array_Search::binarySearch(ReviewBucket& bucket, const string& field1, const string& text1)
+}
+int Array_Search::binarySearch(ReviewBucket& bucket, const string& field1, const string& value1) 
 {
-    if (bucket.size == 0)
-    {
-        return 0;
-    }
-
-    auto getFieldValue = [](const Review& review, const string& field) -> string {
-        if (field == "rating") 
-        {
-            return to_string(review.rating);
-        }
-        return "";
-    };
-
-    ReviewBucket filteredBucket;
-    filteredBucket.capacity = bucket.size;
-    filteredBucket.data = new Review[filteredBucket.capacity];
-    filteredBucket.size = 0;
-
-    int left = 0;
-    int right = bucket.size - 1;
-    int firstMatchIndex = -1;
-
-    while (left <= right) 
-    {
-        int mid = left + (right - left) / 2;
-        string currentValue = getFieldValue(bucket.data[mid], field1);
-
-        if (currentValue == text1) 
-        {
-            firstMatchIndex = mid;
-            right = mid - 1;  
-        } 
-        else if (currentValue < text1) 
-        {
-            left = mid + 1;
-        } 
-        else {
-            right = mid - 1;
-        }
-    }
-
-    if (firstMatchIndex == -1) 
-    {
-        delete[] bucket.data;
-        bucket.data = new Review[bucket.capacity];
-        bucket.size = 0;
-        return 0;
-    }
-
-    int i = firstMatchIndex;
-    while (i > 0 && getFieldValue(bucket.data[i-1], field1) == text1) 
-    {
-        i--;
-    }
-
-    while (i < bucket.size && getFieldValue(bucket.data[i], field1) == text1) 
-    {
-        filteredBucket.data[filteredBucket.size++] = bucket.data[i];
-        i++;
-    }
-
-    int count = filteredBucket.size;
-    delete[] bucket.data;
-    bucket.data = filteredBucket.data;
-    bucket.size = count;
+    int count = 0;
     
+    for (int i = 0; i < bucket.size; i++) 
+    {
+        bool match1 = false;
+
+        if (field1 == "rating")
+        {
+            match1 = (to_string(bucket.data[i].rating) == value1);
+        }
+    
+        if (match1) 
+        {
+            count++;
+        }
+    }
     return count;
 }
 
